bb1750be34d50c97fd8680a3dbf03469da2e2ccf
jaydenisda1 <84245689+jaydenisda1@users.noreply.github.com>
2021-07-23 13:35:39 -0700

    Update Tower.js

--- Tower.js
+++ Tower.js
@@ -3,7 +3,7 @@ class Tower {
     var options = {
       isStatic: true
     };
-    this.ballImage = loadImage("assets/tower.png");
+    this.ballImage = loadImage("tower.png");
     this.width = width;
     this.height = height;
     this.body = Bodies.rectangle(x, y, this.width, this.height, options);
